a8829133f1ec747b6c4f235991ae942ae8979b45
Franco Scuro <franquitoscro@gmail.com>
2022-10-10 16:22:49 -0300

    v1

---
+++ 01/01.js
@@ -0,0 +1,25 @@
+// Escribir un programa que muestre un prompt donde el usuario ingresa un numero, luego muestra otro prompt pidiendo, 
+// otro numero, finalmente el programa muestra una alerta con el resultado de la suma // 
+let numeroUno = parseInt(prompt("Ingrese un numero"));
+let numeroDos = parseInt(prompt("Ingrese otro numero"))
+
+let resultado = numeroUno + numeroDos
+
+alert("El resultado de la suma es " + resultado)
+
+// Crear un programa donde el usuario ingrese un prompt la temperatura en celcius y el programa muestre en una alerta la temperatura en Fahrenheit 
+
+// let celcius = parseInt(prompt("Ingrese temperatura en celcius"));
+// let fahrenheit = ((celcius * 9/5) + 32);
+
+// alert(celcius + " grados Celcius son " + fahrenheit + " grados Fahrenheit")
+
+// Pedir un número a un usuario a través de un prompt y luego dividirlo por 10
+// let numero = parseInt(prompt("Ingrese un numero"));
+// let resultado = (numero / 10);
+
+// alert ( numero + " /" + " 10 = " + resultado)
+
+//Si a = [1,2,3] crear un script que determine de muestre un alert del tipo de dato de a
+// let a = [1,2,3]
+// alert (typeof(a))
