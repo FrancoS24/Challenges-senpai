4a2b1b26491ac2e98bbbe76d0a09c12342d39791
Franco Scuro <franquitoscro@gmail.com>
2022-10-14 11:31:30 -0300

    v1

---
+++ 02/02.js
@@ -0,0 +1,9 @@
+let numero = parseInt(prompt('Ingrese un numero'))
+
+
+    if(numero % 2 == 0) {
+        alert("El " + numero + " es par")
+        
+    } else {
+        alert("El " + numero + " es inpar")
+    }
